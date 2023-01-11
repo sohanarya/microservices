7439f89f5b764658cb2a513f574b05bee5a0f586
Sohan lal <rkjonwal2001@gmail.com>
2023-01-12 01:04:50 +0530

    error genrate

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -30,7 +30,7 @@ public class CurrencyConversionController {
 		uriVariables.put("from",from);
 		uriVariables.put("to",to);
 		logger.info("this is a info message");
-		sohan
+		
 		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
 		("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
 				CurrencyConversion.class, uriVariables);
